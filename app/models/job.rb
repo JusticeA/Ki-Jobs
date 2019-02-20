f5b2dd1adf4e728d34fa64a31a89a18bd1dfa631
Kido Atube <kidoatube@gmail.com>
2019-02-20 15:58:51 +0100

    Add validation to form

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,8 @@
 class Job < ApplicationRecord
+  validates :title, presence: true
+  validates :details, presence: true, length: {minimum: 200}
+  validates :requirement, presence: true, length: {minimum: 10}
+  validates :location, presence: true
+  validates :close_date, presence: true
+  validates :how_to_apply, presence: true
 end
