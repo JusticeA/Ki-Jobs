e5ea02ce0a8895d2d534c0009b1fd3081d52ddfa
Kido Atube <kidoatube@gmail.com>
2019-02-27 17:35:30 +0100

    Update form fields for second user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name, :email, :company_profile, :address])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:company_name, :company_profile, :email, :address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name, :email, :company_profile, :address, :username, :experience])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:company_name, :company_profile, :email, :address, :username, :experience])
   end
+
+
 end
